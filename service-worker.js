789af40284ce5f2199cdb8221ed3d247711be182
singhjagtar0290 <singhjagtar0290@gmail.com>
2022-03-27 13:12:06 +0000

    Deploying to gh-pages from  @ 8d25021541a227f1b00094164c392068a71ddf7a 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e542ab65024f7e57997aa26d0848236d.js"
+  "/personalwebsite/precache-manifest.c1a9c42211e184641efdfce26dfed830.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/personalwebsite/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
